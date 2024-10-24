5b643544f56616b61ee8b5c0468029490b8ecc88
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-10-24 09:28:30 +0200

    initial app

---
+++ react-app/README.md
@@ -0,0 +1,11 @@
+# Setup
+
+npm create vite@latest react-app -- --template react-ts
+
+# Backend
+
+npm install json-server
+
+# React Router
+
+npm install react-router-dom
---
+++ react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import router from './Router';
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
