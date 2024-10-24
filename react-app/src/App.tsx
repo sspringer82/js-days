4e64786ce1fe8a3328707bd962deec23fb193e2c
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-10-24 13:53:56 +0200

    more stuff

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css';
+// import RouterApp from './01-router/App';
+// import RouterApp from './02-actions-loaders/App';
+// import RouterApp from './03-ts-query/App';
 import RouterApp from './04-react-hook-form/App';
 
 function App() {
