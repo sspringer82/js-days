8e193e604b730c0f31c71f06fd17355a1e05f733
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-10-24 14:36:06 +0200

    Aufgabe 4 lösung

--- react-app/src/04-react-hook-form/Router.tsx
+++ react-app/src/04-react-hook-form/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import List from './components/List';
 import Details from './components/Details';
 import NotFound from './components/NotFound';
+import Form from './components/Form';
 
 const router = createBrowserRouter([
   {
--- react-app/src/04-react-hook-form/components/Form.tsx
+++ react-app/src/04-react-hook-form/components/Form.tsx
@@ -1,27 +1,67 @@
 import { useEffect } from 'react';
-import { getUserById } from '../../api/users.api';
-import { useParams } from 'react-router-dom';
+import { createUser, getUserById, updateUser } from '../../api/users.api';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { User } from '../../types/User';
 
 const Form: React.FC = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<User>();
 
   useEffect(() => {
     if (id) {
-      getUserById(id);
+      getUserById(id).then((user) => reset(user));
     }
-  }, [id]);
+  }, [id, reset]);
+
+  async function onSubmit(data: User) {
+    if (data.id) {
+      await updateUser(data);
+    } else {
+      await createUser(data);
+    }
+    navigate('/list');
+  }
+
+  console.log(errors);
 
   return (
     <div>
       <h1>{id ? 'Edit User' : 'Create New User'}</h1>
-      <form>
+      {(errors.firstname || errors.lastname) && (
+        <div>Bitte füllen Sie alle Felder aus!</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstname">First Name:</label>
-          <input type="text" id="firstname" />
+          <label htmlFor="firstname">
+            First Name:
+            <input
+              type="text"
+              {...register('firstname', { required: 'VORNAME!' })}
+            />
+          </label>
+          {errors.firstname && (
+            <div style={{ color: 'red' }}>⚠️{errors.firstname.message}</div>
+          )}
         </div>
         <div>
-          <label htmlFor="lastname">Last Name:</label>
-          <input type="text" id="lastname" />
+          <label htmlFor="lastname">
+            Last Name:
+            <input
+              type="text"
+              {...register('lastname', { required: 'NACHNAME!' })}
+            />
+          </label>
+          {errors.lastname && (
+            <div style={{ color: 'red' }}>⚠️{errors.lastname.message}</div>
+          )}
         </div>
         <button type="submit">{id ? 'Update User' : 'Create User'}</button>
       </form>
