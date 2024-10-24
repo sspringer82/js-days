555e2f3258d919db683f0a9606bea1781a1d3604
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-10-24 11:37:00 +0200

    api functions

--- react-app/src/api/users.api.ts
+++ react-app/src/api/users.api.ts
@@ -9,3 +9,25 @@ export async function getUserById(id: string): Promise<User> {
   const response = await fetch(`http://localhost:3001/users/${id}`);
   return response.json() as Promise<User>;
 }
+
+export async function createUser(user: User): Promise<User> {
+  const response = await fetch('http://localhost:3001/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  return response.json() as Promise<User>;
+}
+
+export async function updateUser(user: User): Promise<User> {
+  const response = await fetch(`http://localhost:3001/users/${user.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  return response.json() as Promise<User>;
+}
